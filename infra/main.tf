d72a7fdb50a316b2b569f7e97d475476795ba36d
AnimalMother83 <mikael.p83@gmail.com>
2022-05-16 00:50:41 +0200

    wip

--- infra/main.tf
+++ infra/main.tf
@@ -47,25 +47,37 @@ resource "random_integer" "ri" {
 }
 
 # Create the Linux App Service Plan
-resource "azurerm_app_service_plan" "appserviceplan" {
+resource "azurerm_service_plan" "plan" {
   name                = "${local.app_name}-${local.env}-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "webapp" {
-  name                = "webapp-${random_integer.ri.result}"
+resource "azurerm_linux_web_app" "app" {
+  name                = "${local.app_name}-${random_integer.ri.result}-${local.env}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
-  source_control {
-    repo_url           = "https://github.com/AnimalMother83/template-svelte-ts"
-    branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    app_command_line = "npm run build"
+    application_stack {
+      node_version = "16-lts"
+    }
+    # source_control {
+    #   repo_url           = "https://github.com/AnimalMother83/template-svelte-ts"
+    #   branch             = "master"
+    #   manual_integration = true
+    #   use_mercurial      = false
+    # }
   }
+}
+
+
+resource "azurerm_app_service_source_control" "src" {
+  app_id   = azurerm_linux_web_app.app.id
+  repo_url = "https://github.com/AnimalMother83/template-svelte-ts"
+  branch   = "master"
 }
